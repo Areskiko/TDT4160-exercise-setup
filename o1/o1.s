da681f31ae24a7c5641dd05ba940d917d7c068f7
Areskiko <39183620+Areskiko@users.noreply.github.com>
2021-09-29 12:06:22 +0200

    Solved o1

--- o1/o1.s
+++ o1/o1.s
@@ -16,25 +16,34 @@ Start:
     LDR R0, =LED_PORT
     LDR R2, =PORT_SIZE
     MUL R0, R0, R2
-    LDR R2, =LED_PIN
-    ADD R0, R0, R2
     LDR R2, =GPIO_BASE
+    ADD R0, R0, R2
+    LDR R2, =GPIO_PORT_DOUT // Bruk av DOUT burde bety at når vi skriver 0b0000000 så resettes det
     ADD R0, R0, R2
 
     // Sett R1 til å være addr til knapp
     LDR R1, =BUTTON_PORT
     LDR R2, =PORT_SIZE
     MUL R1, R1, R2
-    LDR R2, =BUTTON_PIN
-    ADD R1, R1, R2
     LDR R2, =GPIO_BASE
     ADD R1, R1, R2
-   
+    LDR R2, =GPIO_PORT_DIN
+    ADD R1, R1, R2  
 
     Loop:
-        LDR R2, [R1]
-        STR R2, [R0]
-        B Loop
+        LDR R2, [R1] // Last inn verdi fra PE2
+
+        LDR R3, =BUTTON_PIN
+        LDR R4, =LED_PIN
+        SUB R3, R3, R4 // Beregn distanse fra BUTTON_PIN og LED_PIN
+
+        LSR R2, R2, R3 // Shift for å gå fra input pin (9) til output pin (2)
+
+        LDR R3, =0b1111111 // Invertering av bit, uten funker det ikke, kanskje
+        EOR R2, R2, R3     // knappen er normally closed? 
+
+        STR R2, [R0] // Skriv verdien til PB9
+        B Loop // Gå til toppen av loopen
 
 
 NOP // Behold denne på bunnen av fila
