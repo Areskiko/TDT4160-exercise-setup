e95a4cbc2e65bb8e281b208340176c9d58e1fc68
Areskiko <39183620+Areskiko@users.noreply.github.com>
2021-09-29 10:54:52 +0200

    First attempt

--- o1/o1.s
+++ o1/o1.s
@@ -8,7 +8,33 @@
 	
 Start:
 
-    // Skriv din kode her...
+    // R0 er addr til led lys
+    // R1 er addr til knapp
+    // R2 er hjelpe reg
+
+    // Sett R0 til å være addr til led lys
+    LDR R0, =LED_PORT
+    LDR R2, =PORT_SIZE
+    MUL R0, R0, R2
+    LDR R2, =LED_PIN
+    ADD R0, R0, R2
+    LDR R2, =GPIO_BASE
+    ADD R0, R0, R2
+
+    // Sett R1 til å være addr til knapp
+    LDR R1, =BUTTON_PORT
+    LDR R2, =PORT_SIZE
+    MUL R1, R1, R2
+    LDR R2, =BUTTON_PIN
+    ADD R1, R1, R2
+    LDR R2, =GPIO_BASE
+    ADD R1, R1, R2
+   
+
+    Loop:
+        LDR R2, [R1]
+        STR R2, [R0]
+        B Loop
 
 
 NOP // Behold denne på bunnen av fila
